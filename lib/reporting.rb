56489a3b4aa75e66b8874fdcb9c709bc0436a2ac
Joel Van Horn <joel@joelvanhorn.com>
2018-02-11 18:59:41 -0500

    Add default job class configuration

--- lib/reporting.rb
+++ lib/reporting.rb
@@ -1,4 +1,15 @@
 require 'reporting/version'
 
 module Reporting
+  singleton_class.instance_eval do
+    # Configuration options
+    attr_accessor :default_job_class
+
+    def configure
+      block_given? ? yield(self) : self
+    end
+  end
+
+  require_relative 'adapters/kiba/job'
+  self.default_job_class = Adapters::Kiba::Job
 end
