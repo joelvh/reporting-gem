3586e32ff77033c9bcf7ddc8d117abf419f0cb2c
Joel Van Horn <joel@joelvanhorn.com>
2018-02-15 15:38:57 -0500

    Rename pipeline to job

--- lib/reporting/adapters/kiba/worker_proxy.rb
+++ lib/reporting/adapters/kiba/worker_proxy.rb
@@ -1,21 +1,21 @@
 module Reporting
   module Kiba
     class WorkerProxy
-      def initialize(worker, context, pipeline, proxy = nil, &block)
+      def initialize(worker, context, job, proxy = nil, &block)
         @worker   = worker
         @context  = context
-        @pipeline = pipeline
+        @job = job
         # Kiba calls `call` for processors
         # and `each` for sources/destinations
         # and `process` for transforms
         @proxy    = proxy || Proc
-        @block    = ProcBinder.proxy(@pipeline, &block)
+        @block    = ProcBinder.proxy(@job, &block)
       end
 
       # Instance method to simulate class
       def new(*constructor_args)
         @proxy.new do |*call_args|
-          @worker.new(constructor_args, @pipeline, &@block).call(call_args, @context)
+          @worker.new(constructor_args, @job, &@block).call(call_args, @context)
         end
       end
     end
